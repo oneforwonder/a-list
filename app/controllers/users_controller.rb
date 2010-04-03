18ee90231da8e6c98d96179900776302e6eae5db
Rob Nagle <rjn945@gmail.com>
2010-04-02 22:26:34 -0700

    Basic user functionality.
    Listing (index), showing, creating and destroying user functionality and
    views.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,10 @@
 class UsersController < ApplicationController
-  # GET /users
-  # GET /users.xml
+  layout "application"
+  
   def index
     @users = User.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @users }
-    end
   end
-
+  
   # GET /users/1
   # GET /users/1.xml
   def show
