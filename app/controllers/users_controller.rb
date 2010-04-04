30319da5db3fed0b14de36e6c7ec09d7f1a1cf85
Rob Nagle <rjn945@gmail.com>
2010-04-03 18:38:36 -0700

    Merged homepage and sign up page.
    The default route now points to users/new.
    On this page, on the left, is a blurb describing the site.
    On the right, is the sign-up form.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,22 +9,12 @@ class UsersController < ApplicationController
   # GET /users/1.xml
   def show
     @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @user }
-    end
   end
 
   # GET /users/new
   # GET /users/new.xml
   def new
     @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @user }
-    end
   end
 
   # GET /users/1/edit
