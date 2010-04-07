f1df2b864c48ab9bc5a506d8d2a0f1dae8a0399f
Rob Nagle <rob@xi.site>
2010-04-06 22:07:15 -0700

    The sharer's name is now a link.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,13 @@ class ApplicationController < ActionController::Base
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
   
-  helper_method :current_user
+  helper_method :name, :current_user
   
   private
+
+  def name(user)
+    user.name(current_user)
+  end
   
   def authenticate_user
     if !current_user
