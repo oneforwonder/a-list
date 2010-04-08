793ad0f4391bf06f1132880f7472effc3b682809
Rob Nagle <rob@xi.site>
2010-04-07 21:35:50 -0700

    Emails from "Share Notifier", not "shares".

--- app/models/share_mailer.rb
+++ app/models/share_mailer.rb
@@ -5,11 +5,10 @@ class ShareMailer < ActionMailer::Base
   add_template_helper(ApplicationHelper)
   
   def notify(share, sent_at = Time.now)
-    from          "shares@alist.heroku.com"
+    from          "Share Notifier <shares@alist.heroku.com>"
     reply_to      share.link.submitter.email
     recipients    share.recipient.email
     subject       "#{share.link.submitter.name} shared a link with you"
-    #content_type  "text/html"
     body          :share => share
   end
 end
