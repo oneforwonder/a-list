d041518209a59f002c4b033fb56468908038a584
Rob Nagle <rob@xi.site>
2010-04-10 19:15:19 -0700

    Share Mailer sends finish_registration emails.

--- app/models/share_mailer.rb
+++ app/models/share_mailer.rb
@@ -5,10 +5,18 @@ class ShareMailer < ActionMailer::Base
   add_template_helper(ApplicationHelper)
   
   def notify(share, sent_at = Time.now)
+    if share.recipient.activated
+      # Send the user directly to the share.
+      url = share_url(@share.id)
+    else
+      # The user needs to finish creating their account first.
+      url = finish_registration_url(share.recipient.persistence_token)
+    end
+  
     from          "Share Notifier <shares@alist.heroku.com>"
     reply_to      share.link.submitter.email
     recipients    share.recipient.email
     subject       "#{share.link.submitter.name} shared a link with you"
-    body          :share => share
+    body          :share => share, :url => url
   end
 end
