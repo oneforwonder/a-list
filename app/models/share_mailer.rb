d94c1937bf2a9d590657abae5acafa141a8a972c
Matt Hubert <matt@cfxnetworks.com>
2010-04-10 19:07:53 -0700

    Checking in user activation (again).

--- app/models/share_mailer.rb
+++ app/models/share_mailer.rb
@@ -9,6 +9,6 @@ class ShareMailer < ActionMailer::Base
     reply_to      share.link.submitter.email
     recipients    share.recipient.email
     subject       "#{share.link.submitter.name} shared a link with you"
-    body          :share => share
+    body          :share => share, :url => share.recipient.activated ? share_url(@share.id) : finish_activation_url(share.recipient.persistence_token)
   end
 end
