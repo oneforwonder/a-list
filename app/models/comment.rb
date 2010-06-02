e06b46f421b1044b2f71dd94a8d7988975c09a88
Matt Hubert <matt@cfxnetworks.com>
2010-06-02 11:21:14 -0700

    Typo.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :link
   belongs_to :user
   
-  validate_presence_of :content, :link_id, :user_id
+  validates_presence_of :content, :link_id, :user_id
   
   def submitted_time
     Time.parse(created_at.to_s) # TODO: Support timezones.
